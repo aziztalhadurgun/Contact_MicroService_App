d56ce94caed6c71f60f3f6012e929af9018b6022
aziz talha durgun <aziztalhadurgun@gmail.com>
2022-05-23 00:37:16 +0300

    add data access report microservice

---
+++ Contact.Report/DataAccess/Report.cs
@@ -0,0 +1,9 @@
+﻿namespace Contact.Report.DataAccess
+{
+    public class Report
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string Status { get; set; }
+    }
+}
--- Contact.Report/Program.cs
+++ Contact.Report/Program.cs
@@ -1,4 +1,16 @@
+using Contact.Report.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ReportDbContext>(
+    opt =>
+    {
+        opt.EnableSensitiveDataLogging();
+        opt.EnableDetailedErrors();
+        opt.UseNpgsql(builder.Configuration.GetConnectionString("AppDb"));
+    }, ServiceLifetime.Transient
+);
 
 
 var app = builder.Build();
